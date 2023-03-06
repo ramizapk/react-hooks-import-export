9e8f2cb9e33edae2b4a455d515a76afb9d7ce883
Ramiz Ali <ramiz.apk7@gmail.com>
2023-03-06 03:34:41 +0300

    complated assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,26 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import * as RMFunctions from "./parks/RockyMountain";
+import MesaVerde from "./parks/MesaVerde";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
+  console.log(RMFunctions.trees);
+  // => "Aspen and Pine"
+
+  RMFunctions.wildlife();
+  // => "Elk, Bighorn Sheep, Moose"
+
+  RMFunctions.elevation();
+  // => Attempted import error
+
+  return (
+    <div>
+      <MesaVerde />
+    </div>
+  );
 }
+
+
+export default ColoradoStateParks;
